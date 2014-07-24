674d4238d83d3079da3586f33961a42265e32f33
Ueli Scheidegger <uscheidegger@fastforward.ch>
2014-07-24 16:43:30 +0200

    When trying to convert a value to a Calendar object which was not a date, an exception was thrown so far. This blocked the whole node from being converted. Now the problem is logged and and null is returned.

--- src/main/java/ch/fastforward/magnolia/ocm/atomictypeconverter/CalendarTypeConverterImpl.java
+++ src/main/java/ch/fastforward/magnolia/ocm/atomictypeconverter/CalendarTypeConverterImpl.java
@@ -1,39 +1,35 @@
 /**
- * This file Copyright (c) 2010-2011 Magnolia International
- * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ * This file Copyright (c) 2010-2011 Magnolia International Ltd.
+ * (http://www.magnolia-cms.com). All rights reserved.
  *
  *
- * This file is dual-licensed under both the Magnolia
- * Network Agreement and the GNU General Public License.
- * You may elect to use one or the other of these licenses.
+ * This file is dual-licensed under both the Magnolia Network Agreement and the
+ * GNU General Public License. You may elect to use one or the other of these
+ * licenses.
  *
- * This file is distributed in the hope that it will be
- * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
- * Redistribution, except as permitted by whichever of the GPL
- * or MNA you select, is prohibited.
+ * This file is distributed in the hope that it will be useful, but AS-IS and
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT. Redistribution,
+ * except as permitted by whichever of the GPL or MNA you select, is prohibited.
  *
- * 1. For the GPL license (GPL), you can redistribute and/or
- * modify this file under the terms of the GNU General
- * Public License, Version 3, as published by the Free Software
- * Foundation.  You should have received a copy of the GNU
- * General Public License, Version 3 along with this program;
- * if not, write to the Free Software Foundation, Inc., 51
- * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 1. For the GPL license (GPL), you can redistribute and/or modify this file
+ * under the terms of the GNU General Public License, Version 3, as published by
+ * the Free Software Foundation. You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program; if not, write to
+ * the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
  *
- * 2. For the Magnolia Network Agreement (MNA), this file
- * and the accompanying materials are made available under the
- * terms of the MNA which accompanies this distribution, and
- * is available at http://www.magnolia-cms.com/mna.html
+ * 2. For the Magnolia Network Agreement (MNA), this file and the accompanying
+ * materials are made available under the terms of the MNA which accompanies
+ * this distribution, and is available at http://www.magnolia-cms.com/mna.html
  *
- * Any modifications to this file must keep this entire header
- * intact.
+ * Any modifications to this file must keep this entire header intact.
  *
  */
 package ch.fastforward.magnolia.ocm.atomictypeconverter;
 
 import java.util.Calendar;
+import java.util.logging.Level;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.Value;
@@ -41,18 +37,24 @@ import javax.jcr.ValueFactory;
 
 import org.apache.jackrabbit.ocm.exception.IncorrectAtomicTypeException;
 import org.apache.jackrabbit.ocm.manager.atomictypeconverter.AtomicTypeConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Calendar Type Converter.
  *
- * @see org.apache.jackrabbit.ocm.manager.atomictypeconverter.AtomicTypeConverter#getValue(java.lang.Object)
+ * @see
+ * org.apache.jackrabbit.ocm.manager.atomictypeconverter.AtomicTypeConverter#getValue(java.lang.Object)
  * @will
  */
 public class CalendarTypeConverterImpl implements AtomicTypeConverter {
 
+    private Logger log = LoggerFactory.getLogger(CalendarTypeConverterImpl.class);
+
     /**
      *
-     * @see org.apache.jackrabbit.ocm.manager.atomictypeconverter.AtomicTypeConverter#getValue(java.lang.Object)
+     * @see
+     * org.apache.jackrabbit.ocm.manager.atomictypeconverter.AtomicTypeConverter#getValue(java.lang.Object)
      */
     public Value getValue(ValueFactory valueFactory, Object propValue) {
         if (propValue == null) {
@@ -65,7 +67,10 @@ public class CalendarTypeConverterImpl implements AtomicTypeConverter {
 
     /**
      *
-     * @see org.apache.jackrabbit.ocm.manager.atomictypeconverter.AtomicTypeConverter#getObject(javax.jcr.Value)
+     * @param value
+     * @return 
+     * @see
+     * org.apache.jackrabbit.ocm.manager.atomictypeconverter.AtomicTypeConverter#getObject(javax.jcr.Value)
      */
     public Object getObject(Value value) {
         try {
@@ -73,13 +78,22 @@ public class CalendarTypeConverterImpl implements AtomicTypeConverter {
             calendar.setTimeInMillis(value.getDate().getTimeInMillis());
             return calendar;
         } catch (RepositoryException e) {
-            throw new IncorrectAtomicTypeException("Impossible to convert the value : " + value.toString(), e);
+            try {
+                log.error("could not get date value from " + value.getString(), e);
+//            throw new IncorrectAtomicTypeException("Impossible to convert the value : " + value.toString(), e);
+            } catch (IllegalStateException ex) {
+                log.error("could not get date and not even string value from " + value, e);
+            } catch (RepositoryException ex) {
+                log.error("could not get date and not even string value from " + value, e);
+            }
+            return null;
         }
     }
 
     /**
      *
-     * @see org.apache.jackrabbit.ocm.manager.atomictypeconverter.AtomicTypeConverter#getStringValue(java.lang.Object)
+     * @see
+     * org.apache.jackrabbit.ocm.manager.atomictypeconverter.AtomicTypeConverter#getStringValue(java.lang.Object)
      */
     public String getXPathQueryValue(ValueFactory valueFactory, Object object) {
         Calendar calendar = (Calendar) object;
